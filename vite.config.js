9e0893d617ef8a4012ec24a83f93fc0616fbac3d
Ardian Pratama <ardnpratm@gmail.com>
2025-03-29 23:10:45 +0700

    feat(seo): generate sitemap.xml using vite-plugin-sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sitemap({
+      hostname: "https://ardian-pratama.vercel.app",
+      changefreq: "daily",
+      priority: 1.0,  
+    }),],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
