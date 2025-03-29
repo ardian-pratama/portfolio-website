b58838e969c42be1f03e331311276d43b3afa178
Ardian Pratama <ardnpratm@gmail.com>
2025-03-29 22:29:17 +0700

    feat(error): add Not Found page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <div className='fixed inset-0 flex items-center justify-center'>
+      <p className='text-primary'>Page not found</p>
+    </div>
+  );
+}
